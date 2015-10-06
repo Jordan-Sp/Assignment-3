e5c14c4ec5be5a12e1a6eced0f599a84bcd15dff
Jordan Sparr <concretedragon34@gmail.com>
2015-10-05 20:49:38 -0600

    Edited and did the work

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // get all of our list items
     // 1. Select all the items to filter
-    var dataList =
+    var dataList = require('./app');
     //setup click event handlers on our checkboxes
     // 2. Select all the checkboxes to watch
-    var checkBoxes =
+    var checkBoxes = dataList.querySelectorAll('checkbox');
 
     for (var i = 0; i < checkBoxes.length; i++) {
         checkBoxes[i].checked = true;
         // 3. Add an event listener for each checkbox
         // each checkbox should use filterItems()
-
+        checkBoxes.addEventListener('click', filterItems());
     }
     // event handler
     function filterItems() {
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
         for (var i = 0; i < dataList.length; i++) {
             var item = dataList[i];
             // 4. show or hide the item if the item attribute "data-category" == value
-            if() {
+            if(item[i].querySelector("data-category")==value) {
                 if(show){
                     item.style.display = "block";
                 }else{
